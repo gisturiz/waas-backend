4ebf49d8b2061eaccb7e2dc4249aa8d09329f21e
Gustavo <gustavo.isturiz@coinbase.com>
2023-03-21 16:02:57 -0700

    redid schema

--- models/models.js
+++ models/models.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const modelsSchema = new mongoose.Schema({
-    poolName: String,
-    deviceID: String,
     deviceGroupName: String,
-    address: String,
+    Addresses: [
+        {
+            address: String,
+            network: String
+        }
+    ],
+    deviceId: String,
+    username: String
 });
 
 module.exports = mongoose.model('models', modelsSchema);
