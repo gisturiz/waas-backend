823ced5961878f0cf69200aaf2e9600b972031c5
Gustavo Isturiz <isturizgustavo@gmail.com>
2023-03-22 09:47:49 -0700

    added cors, helmet

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const app = express();
 const moongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
+const helmet = require('helmet');
+const cors = require('cors');
 
-main()
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+const app = express();
+
+// Uses
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', routes);
 
 async function main() {
     await moongoose.connect('mongodb://localhost:27017/test');
 };
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+main()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
 
 // Define port to assigned or 3000
 const PORT = process.env.PORT || 3000;
@@ -23,9 +31,7 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
 
+// Basic path
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-app.use('/api', routes);
-
