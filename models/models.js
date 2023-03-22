6598fd6604401b857bfad83421b890e5c3b5116a
Gustavo <gustavo.isturiz@coinbase.com>
2023-03-22 15:24:17 -0700

    changed schema

--- models/models.js
+++ models/models.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 
 const modelsSchema = new mongoose.Schema({
     deviceGroupName: String,
-    Addresses: [
-        {
-            address: String,
-            network: String
-        }
-    ],
+    address: String,
+    deviceData: String,
     deviceId: String,
     username: String
-});
+  });
 
 module.exports = mongoose.model('models', modelsSchema);
